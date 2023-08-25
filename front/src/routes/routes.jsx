9d5c8e2a40de1e1070e66cecab4be7163b057177
sehyun <e6d1fe@gmail.com>
2023-08-26 01:24:57 +0900

    feat: 로그인 페이지 생성

--- front/src/routes/routes.jsx
+++ front/src/routes/routes.jsx
@@ -2,11 +2,13 @@ import React from "react"
 import { Route, Routes } from "react-router-dom"
 import Main from "./../pages/Main"
 import Start from "../pages/Start"
+import Login from "../pages/Login"
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<Start />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   )
 }
