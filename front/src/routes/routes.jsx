73d122350891224fb1cad1ab307d2a265ef6b430
sehyun <e6d1fe@gmail.com>
2023-08-26 06:04:59 +0900

    feat: randomdetail 페이지 생성, 기능 구현

--- front/src/routes/routes.jsx
+++ front/src/routes/routes.jsx
@@ -9,6 +9,7 @@ import Login from "../pages/Login"
 import TravelParticipants from "../pages/TravelParticipants"
 import DrinkParticipants from "../pages/DrinkParticipants"
 import RandomParticipants from "../pages/RandomParticipants"
+import RandomDetail from "../pages/RandomDetail"
 
 function Router() {
   return (
@@ -22,6 +23,7 @@ function Router() {
       <Route path="/travel" element={<Travel />} />
       <Route path="/travel/participants" element={<TravelParticipants />} />
       <Route path="/random/participants" element={<RandomParticipants />} />
+      <Route path="/random/detail" element={<RandomDetail />} />
       <Route path="/login" element={<Login />} />
     </Routes>
   )
